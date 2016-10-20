b74ba0d954877b537a7f7c5bc609249e2f76ae95
Dan Laugharn <dlaugharn@MacBook-Air.local>
2016-10-20 10:58:27 -0500

    Adds Tweaks

--- resources/assets/js/content/campaigns.js
+++ resources/assets/js/content/campaigns.js
@@ -11,15 +11,15 @@ export default {
             cardClasses: '--third',
             cards: [
                 {
-                    description: 'Matt Saracen',
+                    description: 'The shy and stammering sophomore who is quarterback/wide receiver of the Dillon Panthers and becomes the boyfriend of Julie Taylor. He wore the jersey number 7 which Tim Riggins frequently referred to him by.',
                     title: 'Clear Eyes'
                 },
                 {
-                    description: 'Tim Riggins',
+                    description: 'The fullback/running back of the Dillon Panthers, he wore the jersey number 33. He is the troubled best friend of Jason Street and ex-boyfriend of Tyra Collette. He later dates Lyla Garrity, with whom he has an off and on relationship.',
                     title: 'Full Hearts'
                 },
                 {
-                    description: 'Coach Eric Taylor',
+                    description: 'The varsity football coach of the Dillon Panthers. The character is based on the real-life coach Gary Gaines. Like Gaines, Taylor is the new coach who is having a difficult time adjusting to the intensity that Dillon shows for football.',
                     title: 'Can\'t Lose'
                 }
             ]
